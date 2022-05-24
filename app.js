a1c6a71cfff6d546796166c2f785a021e168550c
Dinesh836 <103949855+Dinesh836@users.noreply.github.com>
2022-05-24 21:33:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express=require('express');
+const path=require('path');
+const bodyParser=require('body-parser');
+const app=express();
+
+const adminRoute=require('./routes/admin');
+const shopRoute=require('./routes/shop');
+const contactRoute=require('./routes/contact');
+const submitRoute=require('./routes/success');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(adminRoute);
+app.use(shopRoute);
+app.use(contactRoute);
+app.use(submitRoute);
+
+app.use((req,res,next)=>{
+
+    res.status(404).sendFile(path.join(__dirname , 'views' , '404.html'));
+})
+
+app.listen(3000);
